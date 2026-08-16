252a7923e24676006682f386a4d2f88269075995
mohitparmar3390 <mohitparmar332609@gmail.com>
2026-08-16 19:23:09 +0530

    Set up Snake and Ladder game with die roll function
    Initialize Snake and Ladder game structure with basic setup.

--- SnakeLadderProgram/snakeladder.c
+++ SnakeLadderProgram/snakeladder.c
@@ -1,1 +1,24 @@
+// C Program use Snake and Ladder Program used Header files Include
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+// Function to roll a six-sided die ... 1 2 3 4 5 6
+int rollDie() { return rand() % 6 + 1; }
+
+// global variables store postions of player1 and player2
+int player1 = 0, player2 = 0;
+
+
+int main()
+{
+    srand(time(0)); 
+    int currentPlayer = 1;
+    int won = 0;
+    printf("Snake and Ladder Game\n");
+
+    while (!won) {
+    }
+
+    return 0;
+}
